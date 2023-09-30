6cbae7f6dfccaec46e12dd3c1326d8b01354a85f
Sara Camila Laura Manzaneda <SCamilaLauraM@outlook.es>
2023-09-30 19:53:18 -0400

    add banner

---
+++ packages/nextjs/pages/invest-in-project.tsx
@@ -0,0 +1,5 @@
+const InvestOnProject = () => {
+  return <h1>Invest on project</h1>;
+};
+
+export default InvestOnProject;
---
+++ packages/nextjs/pages/register-water-entity.tsx
@@ -0,0 +1,4 @@
+const ChooseWatterEntity = () => {
+  return <h1>water entity</h1>;
+};
+export default ChooseWatterEntity;
