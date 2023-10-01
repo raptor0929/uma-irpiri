f8621ced1b68903aab24af15f02a3ea05d91d243
Sara Camila Laura Manzaneda <SCamilaLauraM@outlook.es>
2023-09-30 21:33:20 -0400

    image cards

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,13 +1,85 @@
 import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { ReusableCard } from "~~/components/common/Card";
+import { BannerCard } from "~~/components/common/BannerCard";
+import { ImageTextCard } from "~~/components/common/ImageTextCard";
 
+const cards = [
+  {
+    index: 1,
+    title: "bad river",
+    image: "/Assets/",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+    waterPurity: 45,
+    saving: 80,
+  },
+  {
+    index: 2,
+    title: "bad river",
+    image: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+    waterPurity: 45,
+    saving: 80,
+  },
+  {
+    index: 3,
+    title: "bad river",
+    image: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+    waterPurity: 45,
+    saving: 80,
+  },
+  {
+    index: 4,
+    title: "bad river",
+    image: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+    waterPurity: 45,
+    saving: 80,
+  },
+  {
+    index: 5,
+    title: "bad river",
+    image: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+    waterPurity: 45,
+    saving: 80,
+  },
+  {
+    index: 6,
+    title: "bad river",
+    image: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+    waterPurity: 45,
+    saving: 80,
+  },
+];
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow pt-5">
-        <ReusableCard />
+        <div className="pb-5">
+          <BannerCard />
+        </div>
+        <div className="flex justify-center items-start  flex-row flex-wrap">
+          {cards.map(x => (
+            <div key={x.index}>
+              <ImageTextCard
+                title={x.title}
+                image={x.image}
+                description={x.description}
+                waterPurity={x.waterPurity}
+                saving={x.saving}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
