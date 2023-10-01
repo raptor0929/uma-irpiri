20d47e739cfa932cf25ee3219104b55d1c8cd4a6
Camila13L <scamilalauram@outlook.es>
2023-10-01 09:45:47 -0400

    fixed form

--- packages/nextjs/components/CreateSbt.tsx
+++ packages/nextjs/components/CreateSbt.tsx
@@ -1,49 +1,71 @@
-// import { useReducer } from "react";
+import { useReducer } from "react";
 import Link from "next/link";
 import { ReusableForm } from "./common/ReusableForm";
-import { MintSbt } from "~~/data/mintSbt";
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
+import { FormType } from "~~/types/form.type";
 
-// interface FormState {
-//   name: string;
-//   email: string;
-//   country: string;
-// }
+interface FormState {
+  name: string;
+  email: string;
+  country: string;
+}
 
-// type FormAction = { type: "SET_FIELD"; field: string; value: string } | { type: "RESET" };
+type FormAction = { type: "SET_FIELD"; field: string; value: string } | { type: "RESET" };
 
-// const initialState: FormState = {
-//   name: "",
-//   email: "",
-//   country: "",
-// };
+const initialState: FormState = {
+  name: "",
+  email: "",
+  country: "",
+};
 
-// const formReducer = (state: FormState, action: FormAction): FormState => {
-//   switch (action.type) {
-//     case "SET_FIELD":
-//       return {
-//         ...state,
-//         [action.field]: action.value,
-//       };
-//     case "RESET":
-//       return initialState;
-//     default:
-//       return state;
-//   }
-// };
+const formReducer = (state: FormState, action: FormAction): FormState => {
+  switch (action.type) {
+    case "SET_FIELD":
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 const CreateSbt = () => {
-  // const [formData, dispatch] = useReducer(formReducer, initialState);
+  const [formData, dispatch] = useReducer(formReducer, initialState);
+  const MintSbt: FormType[] = [
+    {
+      label: "Organization's name",
+      type: "text",
+      value: formData.name,
+      name: "name",
+    },
+    {
+      label: "Organization's email",
+      type: "text",
+      value: formData.email,
+      name: "email",
+    },
+    {
+      label: "Country",
+      type: "text",
+      value: formData.country,
+      name: "country",
+    },
+  ];
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    dispatch({ type: "SET_FIELD", field: name, value });
+  };
 
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = e.target;
-  //   dispatch({ type: "SET_FIELD", field: name, value });
-  // };
-
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   console.log("Datos del formulario:", formData);
-  // };
-
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Datos del formulario:", formData);
+  };
+  const setDisabled = () => {
+    return formData.country !== "" && formData.email !== "" && formData.name !== "";
+  };
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="fixed inset-0 bg-black opacity-50"></div>
@@ -52,11 +74,18 @@ const CreateSbt = () => {
         <h1 className="text-2xl mt-4 text-center">CREA TU SBT</h1>
         <p className="w-72 text-center my-0">Necesitamos que crees un SBT para registrar las propuestas de proyectos</p>
         <div className="mx-auto">
-          <ReusableForm formData={MintSbt} />
+          <ReusableForm
+            formData={MintSbt}
+            onSubmit={handleSubmit}
+            onChange={handleInputChange}
+            buttonLabel="Next"
+            disabled={!setDisabled()}
+          />
         </div>
-        <div className="ml-8 mt-[-20px]">
+        <div className="inline-flex ml-8 mt-[20px]">
           <Link className="text-red-500 hover:text-red-700 underline" href={"/"}>
             Go Back
+            <ArrowLeftIcon className="h-4 w-4" />
           </Link>
         </div>
       </div>
--- packages/nextjs/pages/register-water-entity.tsx
+++ packages/nextjs/pages/register-water-entity.tsx
@@ -1,15 +1,100 @@
+import { useState } from "react";
+import Image from "next/image";
 import CreateSbt from "~~/components/CreateSbt";
 import { ReusableForm } from "~~/components/common/ReusableForm";
-import { RegisterProjectForm } from "~~/data/registerProjectForm";
-import Image from "next/image";
+import { FormType } from "~~/types/form.type";
+
+interface FormValues {
+  community: string;
+  location: string;
+  contamination: number;
+  amount: number;
+  contaminationType: number;
+}
 const ChooseWatterEntity = () => {
+  const [formData, setFormData] = useState<FormValues>({
+    community: "",
+    location: "",
+    amount: 0,
+    contamination: 0,
+    contaminationType: 1,
+  });
+
+  const RegisterProjectForm: FormType[] = [
+    {
+      label: "Community Name",
+      type: "text",
+      name: "community",
+      value: formData.community,
+    },
+    {
+      label: "Location",
+      type: "text",
+      name: "location",
+      value: formData.location,
+    },
+    {
+      label: "Amount Needed (USD)",
+      type: "number",
+      name: "amount",
+      value: formData.amount,
+    },
+    {
+      label: "Water Contamination Percentage (%)",
+      type: "number",
+      name: "contamination",
+      value: formData.contamination,
+    },
+    {
+      label: "Contamination Type",
+      type: "select",
+      name: "contaminationType",
+      value: formData.contaminationType,
+      options: [
+        {
+          id: 1,
+          name: "Physical",
+          value: 1,
+        },
+        {
+          id: 2,
+          name: "Chemical",
+          value: 2,
+        },
+      ],
+    },
+  ];
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  const setDisable = () => {
+    return (
+      formData.amount !== 0 && formData.community !== "" && formData.contamination !== 0 && formData.location !== ""
+    );
+  };
   return (
     <div className="flex justify-center flex-row flex-wrap py-5">
-      <div className="bg-white shadow-md mt-5 rounded-lg" style={{ height: "32.3rem", width: "32rem" }}>
+      <div className="bg-white shadow-md mt-5 rounded-lg" style={{ height: "42rem", width: "32rem" }}>
         <h2 className=" text-center text-2xl pt-5">Register Water Entity</h2>
-        <ReusableForm formData={RegisterProjectForm} buttonLabel="Register" />
+        <ReusableForm
+          formData={RegisterProjectForm}
+          buttonLabel="Register"
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          disabled={!setDisable()}
+        />
       </div>
-      <div className="m-5 shadow-md bg-white rounded-lg p-6 cursor-crosshair">
+      <div className="flex m-5 shadow-md bg-white rounded-lg p-6 cursor-crosshair" style={{ height: "42rem" }}>
         <Image className="rounded-lg " src="/Assets/map.png" alt="" width={500} height={0} />
       </div>
       <CreateSbt />
