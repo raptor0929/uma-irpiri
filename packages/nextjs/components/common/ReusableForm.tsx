8afa9aded1864a0c4a70a0f9b91a6b7eade9b170
Camila13L <scamilalauram@outlook.es>
2023-09-30 23:41:23 -0400

    select added

--- packages/nextjs/components/common/ReusableForm.tsx
+++ packages/nextjs/components/common/ReusableForm.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { Dropdown } from "./Dropdown";
 import { FormType } from "~~/types/form.type";
 
 type Props = {
@@ -13,22 +14,40 @@ export const ReusableForm: FC<Props> = ({ formData, onSubmit, onChange }) => {
       <div className="row d-flex justify-content-around align-items-center">
         <div className="row d-flex justify-content-between align-items-center py-5 col-xxl-9 col-lg-10 col-xl-10">
           {formData.map((field: FormType) => (
-            <div key={field.name} className={`' mt-md-3`}>
-              <label className="form-label text-muted fs-6 fw-bold" htmlFor={field.name}>
+            <div key={field.name} className="">
+              <label className="block text-gray-700 text-sm font-bold my-2" htmlFor={field.name}>
                 {field.label}
               </label>
               <input
                 type={field.type}
-                className="form-control form-control-lg form-control-solid"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 name={field.name}
                 value={field.value.toString()}
+                id={field.name}
                 //onChange={() => {}}
               />
+              {field.type === "select" && (
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold my-2" htmlFor={field.name}>
+                    {field.label}
+                  </label>
+                  <Dropdown
+                    items={!!field.options ? field.options : []}
+                    name={field.name}
+                    value={0}
+                    onChange={() => {}}
+                  />
+                </div>
+              )}
             </div>
           ))}
         </div>
-        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-          Button
+        <button
+          className=" bg-sky-300 hover:bg-sky-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          disabled={false}
+        >
+          Invest
         </button>
         {/* <GenericButton
           iconPath='/media/icons/duotune/general/gen031.svg'
